5cfce01f5d12b6c6be0b70d52db9b68be660745c
JeremyJiWZ <mr.ji@icloud.com>
2016-06-30 23:17:51 +0800

    plc

--- plc.c
+++ plc.c
@@ -1,32 +1,32 @@
 #include "plc.h"
 
 #define PLC_STRLEN 9
-//The BLE device
-int ble_fd = 0;
+//The PLC device
+int plc_fd = 0;
 
 //For controller to check
 //blueteeth中断
 //extern int flag_bt;                        //for controller to check
-int head4ble, rear4ble;
-int head2ble, rear2ble;
-char buf4ble[MAX_BUF_SIZE][BLE_SIZE];
-char buf2ble[MAX_BUF_SIZE][BLE_SIZE];
+int head4plc, rear4plc;
+int head2plc, rear2plc;
+char buf4plc[MAX_BUF_SIZE][PLC_SIZE];
+char buf2plc[MAX_BUF_SIZE][PLC_SIZE];
 struct termios stNew;
 struct termios stOld;
 //Open Port & Set Port
-int serial_init_ble(){
-    ble_fd = open(BLE_DEVICE, O_RDWR|O_NOCTTY|O_NDELAY);
-    if(-1 == ble_fd)
+int serial_init_plc(){
+    plc_fd = open(PLC_DEVICE, O_RDWR|O_NOCTTY|O_NDELAY);
+    if(-1 == plc_fd)
     {
         perror("Open Serial Port Error!\n");
         return -1;
     }
-    if( (fcntl(ble_fd, F_SETFL, 0)) < 0 )
+    if( (fcntl(plc_fd, F_SETFL, 0)) < 0 )
     {
         perror("Fcntl F_SETFL Error!\n");
         return -1;
     }
-    if(tcgetattr(ble_fd, &stOld) != 0)
+    if(tcgetattr(plc_fd, &stOld) != 0)
     {
         perror("tcgetattr error!\n");
         return -1;
@@ -34,8 +34,8 @@ int serial_init_ble(){
     stNew = stOld;
     cfmakeraw(&stNew);//将终端设置为原始模式，该模式下所有的输入数据以字节为单位被处理
     //set speed
-    cfsetispeed(&stNew, BLE_BAUDRATE);//9600
-    cfsetospeed(&stNew, BLE_BAUDRATE);
+    cfsetispeed(&stNew, PLC_BAUDRATE);//9600
+    cfsetospeed(&stNew, PLC_BAUDRATE);
     //set databits
     stNew.c_cflag |= (CLOCAL|CREAD);
     stNew.c_cflag &= ~CSIZE;
@@ -48,25 +48,25 @@ int serial_init_ble(){
     stNew.c_cc[VTIME]=0;    //指定所要读取字符的最小数量
     stNew.c_cc[VMIN]=1; //指定读取第一个字符的等待时间，时间的单位为n*100ms
                 //如果设置VTIME=0，则无字符输入时read（）操作无限期的阻塞
-    tcflush(ble_fd,TCIFLUSH);  //清空终端未完成的输入/输出请求及数据。
-    if( tcsetattr(ble_fd,TCSANOW,&stNew) != 0 )
+    tcflush(plc_fd,TCIFLUSH);  //清空终端未完成的输入/输出请求及数据。
+    if( tcsetattr(plc_fd,TCSANOW,&stNew) != 0 )
     {
         perror("tcsetattr Error!\n");
         return -1;
     }
-    return ble_fd;
+    return plc_fd;
 }
 
 //return the len of the data
-int ble_read(int ble_fd){
+int plc_read(int plc_fd){
     int nRet=0;
     int flag=1;
     int len=0;
-    char str[BLE_SIZE];
+    char str[PLC_SIZE];
     char tmp[16];
     while(flag||nRet!=0)
     {
-        nRet = read(ble_fd, tmp, 1);
+        nRet = read(plc_fd, tmp, 1);
         if(tmp[0]>='a'&&tmp[0]<='z')
             nRet=1;
         else if(tmp[0]>='0'&&tmp[0]<='9')
@@ -85,14 +85,14 @@ int ble_read(int ble_fd){
             break;
         }
         if(len>=PLC_STRLEN){
-            strcpy(buf4ble[head4ble],str);
+            strcpy(buf4plc[head4plc],str);
             printf("str Data: %s\n", str);
-            if(head4ble==MAX_BUF_SIZE)
-                    head4ble=0;
+            if(head4plc==MAX_BUF_SIZE)
+                    head4plc=0;
             else
-                head4ble++;
-            if(head4ble==rear4ble)
-                rear4ble++;
+                head4plc++;
+            if(head4plc==rear4plc)
+                rear4plc++;
             print_buf();
             len=0;
             nRet=0;
@@ -102,9 +102,9 @@ int ble_read(int ble_fd){
 }
 
 //write to the serial port
-int ble_write(int ble_fd, int len)
+int plc_write(int plc_fd, int len)
 {
-    return write(ble_fd, buf2ble, len);
+    return write(plc_fd, buf2plc, len);
 }
 
 void print_buf()
@@ -112,32 +112,32 @@ void print_buf()
     int i=0;
     for(i=0;i<8;i++)
     {
-        printf("[%d]:%s\n", i, buf4ble[i]);
+        printf("[%d]:%s\n", i, buf4plc[i]);
     }
 }
 
-//BLE module
-int BLE_init()
+//PLC module
+int PLC_init()
 {
-    bzero(buf4ble, BLE_SIZE);
-    bzero(buf2ble, BLE_SIZE);
-    return serial_init_ble();
+    bzero(buf4plc, PLC_SIZE);
+    bzero(buf2plc, PLC_SIZE);
+    return serial_init_plc();
 }
 
-//send data to ble
-void send2ble()
+//send data to plc
+void send2plc()
 {
 
 }
 
-//get data from ble
-int get4ble(char* tmp)
+//get data from plc
+int get4plc(char* tmp)
 {
-    if(rear4ble!=head4ble)
+    if(rear4plc!=head4plc)
     {
-        strcpy(tmp, buf4ble[rear4ble++]);
-        if(rear4ble==MAX_BUF_SIZE)
-            rear4ble=0;
+        strcpy(tmp, buf4plc[rear4plc++]);
+        if(rear4plc==MAX_BUF_SIZE)
+            rear4plc=0;
         return 1;
     }
     else
@@ -147,17 +147,17 @@ int get4ble(char* tmp)
 /*
 int b(int argc, char **argv)
 {
-    ble_fd=BLE_init();
-    if( ble_fd == -1 )
+    plc_fd=PLC_init();
+    if( plc_fd == -1 )
     {
         perror("SerialInit Error!\n");
         return -1;
     }
 
-    ble_read(ble_fd);
+    plc_read(plc_fd);
 
     print_buf();
 
-    close(ble_fd);
+    close(plc_fd);
     return 0;
 }*/
--- plc.h
+++ plc.h
@@ -10,39 +10,39 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h>
-#define BLE_BAUDRATE B9600 ///Baud rate : 115200
-#define BLE_DEVICE "/dev/ttyAMA0"
-#define BLE_SIZE 512
+#define PLC_BAUDRATE B9600 ///Baud rate : 115200
+#define PLC_DEVICE "/dev/ttyAMA0"
+#define PLC_SIZE 512
 #define MAX_BUF_SIZE 512
 
-//The BLE device
-extern int ble_fd;
+//The PLC device
+extern int plc_fd;
 
 //For controller to check
 //blueteeth中断
 //extern int flag_bt;                        //for controller to check
-extern int head4ble, rear4ble;
-extern int head2ble, rear2ble;
-extern char buf4ble[MAX_BUF_SIZE][BLE_SIZE];
-extern char buf2ble[MAX_BUF_SIZE][BLE_SIZE];
+extern int head4plc, rear4plc;
+extern int head2plc, rear2plc;
+extern char buf4plc[MAX_BUF_SIZE][PLC_SIZE];
+extern char buf2plc[MAX_BUF_SIZE][PLC_SIZE];
 
 //init serial
-int serial_init_ble();
+int serial_init_plc();
 
 //return the len of the data
-int ble_read(int ble_fd);
+int plc_read(int plc_fd);
 
 //write to the serial port
-int ble_write(int ble_fd, int len);
+int plc_write(int plc_fd, int len);
 
-//BLE module
-int BLE_init();
+//PLC module
+int PLC_init();
 
-//send data to ble
-void send2ble();
+//send data to plc
+void send2plc();
 
-//get data from ble
-int  get4ble(char *tmp);
+//get data from plc
+int  get4plc(char *tmp);
 
 void print_buf();
 
